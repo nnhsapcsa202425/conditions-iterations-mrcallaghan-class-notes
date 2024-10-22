8a104e05bfa9ca4fc0a447701b0e26d382b45da4
Matt Callaghan <mcallaghan@naperville203.org>
2024-10-22 15:09:28 -0500

    Wrote notes on string comparison

--- Conditions.java
+++ Conditions.java
@@ -84,6 +84,78 @@ public class Conditions
             System.out.println("coin is heads");
             System.out.println("better luck next time...");
 
+    }
+    
+    public static boolean doublesAreEqual(double num1, double num2)
+    {
+        /*
+         * Be careful using the equality operator (i.e., ==) with doubles.  It's checking if all binary digits
+         * match, and due to floating point arithmetic and rounding, it may not give us what we want.
+         * 
+         * Instead, check if doubles are "close enough" using a epsilon value.
+         */
+        
+        final double EPSILON = 1e-6;
+        
+        if (Math.abs(num1 - num2) < EPSILON)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        // don't have to use the if/else here
+        //return Math.abs(num1 - num2) < EPSILON;
+    }
+    
+    public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter two words (with a space inbetween words): ");
+        String str1 = s.next();
+        String str2 = s.next();
+        
+        /*
+         * The equality operator (==) returns true if the object references are equal. In other words,
+         *  the objects store the same location in computer memory.  This is not the same as the same 
+         *  sequence of characters.
+         */
+        if (str1 == str2)
+        {
+            System.out.println("The string references are equal.");
+        }
+        else
+        {
+            System.out.println("The string references are NOT equal.");
+        }
+        
+        /*
+         * The equals method return true if the two objects referenced by the variables are "equal".
+         *      What "equal" means gets determined by the class.  For Strings, equeality means the Strings
+         *      contain the same sequence of characters.
+         *  
+         */
+        if (str1.equals(str2))
+        {
+            System.out.println("The strings are equal (same sequence of characters).");
+        }
+        else
+        {
+            System.out.println("The strings NOT are equal (different sequence of characters).");
+        }
+        
+        /*
+         * If we want to check if strigns are not equal, we can use the logical complement operator (i.e., !)
+         */
+        if (! str1.equals(str2))
+        {
+            System.out.println("The strings are NOT equal.");
+        }
+        
+        
+        
         
     }
 }
