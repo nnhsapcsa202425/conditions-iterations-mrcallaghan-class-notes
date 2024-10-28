8d2997a8361c9286b22588250a83534bb3fb7ddd
Matt Callaghan <mcallaghan@naperville203.org>
2024-10-28 08:13:47 -0500

    added string compare activity

--- Conditions.java
+++ Conditions.java
@@ -181,6 +181,47 @@ public class Conditions
         
     }
     
+    /*
+     * 1) word1 is lexicographically greater than "aaa"
+       2) word1 is lexicographically equal to word2
+       3) word1 is lexicographically less than word2
+       4) word1 and word2 have the same three-letter prefix    
+     */
+    public static void stringCompare()
+    {
+        String word1 = "catalog";
+        String word2 = "cat";
+        
+        if(word1.compareTo("aaa") > 0)
+        {
+            System.out.println("condition #1 is true");
+        }
+        
+        if(word1.equals(word2))
+        // if(word1.compareTo(word2) == 0)
+        {
+            System.out.println("condition #2 is true");
+        }
+        else
+        {
+            System.out.println("condition #2 is false");
+        }
+        
+        if(word1.compareTo(word2) < 0)
+        {
+            System.out.println("condition #3 is true");
+        }
+        else
+        {
+            System.out.println("condition #3 is false");
+        }
+        
+        if(word1.substring(0, 3).equals(word2.substring(0, 3)))
+        {
+            System.out.println("condition #4 is true");
+        }
+    }
+    
     
     
     
