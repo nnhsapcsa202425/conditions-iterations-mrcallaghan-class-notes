e5fe88d6dbcd88b7cade10b0fc5e695bd41e19a3
Matt Callaghan <mcallaghan@naperville203.org>
2024-11-04 14:51:54 -0600

    Wrote notes on for loops and demonstrated debugger

--- Iterations.java
+++ Iterations.java
@@ -58,9 +58,69 @@ public class Iterations
     }
     
     
+    public static void forExample()
+    {
+        /*
+         * for loop:
+         * 
+         *  Three part to the for statment:
+         *      1. initialization - executed once
+         *      2. condition - evaluated at the start of each iteration
+         *      3. update of the loop variable - executed at the end of each iteration
+         */
+        
+        
+        // useful style for debugging, but less common
+        for (int count = 1;             // initiailization
+                 count <= 5;            // condition
+                 count++)               // update loop variable
+        {
+            System.out.println("count: " + count);
+            int local = 0;
+        }
+        
+        System.out.println("done.");
+        /*
+         * Variables declared within the for statment or for loop are scoped to the for loop
+         */
+        //System.out.println(count);
+        //System.out.println(local);
+        
+        // more common style, all on one line
+        for (int count = 1; count <= 5; count++)   
+        {
+            System.out.println("count: " + count);
+        }
+    }
     
     
     
+    
+    
+    
+    
+    
+    public static void forExample2()
+    {
+        
+        /*
+         * The infamous off-by-one error is common with for loops incrementing one too many or one too few times.
+         * 
+         * Carfully ask:
+         *      - Should the intial value start at 1 or 0?
+         *      - Should the condition be inclusive or exclusive (<, <=)?
+         */
+        int i = 0;
+        for (   ;
+             i <= 5;
+             i++)
+        {
+            // intent is to print 5 plus signs
+            System.out.println("+");
+        }
+        
+        System.out.println("done.");
+    }
     
     
     
